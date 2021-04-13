347b5bb3fbb9f6018933a72ff7fec51e6f64c0e8
AndrewDKahn <18akahn.stem@gmail.com>
2021-04-13 15:03:31 -0400

    clock backend functional

---
+++ src/main/java/project_16x16/ui/CountdownClock.java
@@ -0,0 +1,32 @@
+package project_16x16.ui;
+
+public class CountdownClock {
+    private long startTime, timeLimit, elapsedSec;
+
+    public CountdownClock(long timeLimit){
+        startTime = System.currentTimeMillis();
+        this.timeLimit = timeLimit;
+    }
+
+    public long getTimeLimit() {
+        return timeLimit;
+    }
+
+    public long calculateTime(){ //calculates time elapsed in seconds
+        elapsedSec = (System.currentTimeMillis() - startTime)/1000;
+        return elapsedSec;
+    }
+
+    public static void main(String args[]){
+        System.out.println("TEST");
+        CountdownClock c = new CountdownClock(10);
+        while(c.calculateTime() <= c.getTimeLimit()){
+            System.out.println(c.getTimeLimit()-c.calculateTime());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
