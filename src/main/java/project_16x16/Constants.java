9c87ab05b96e01e383014b3dd8ae34ffe8fce521
Wayne Zheng <wnz6klj@gmail.com>
2021-04-21 14:08:21 -0400

    Revert "Merge pull request #47 from CIS-SoftwareDesign-S21/StoryRevision"
    This reverts commit b9283b966ada678bf57ecc8de3dd01742a164535, reversing
    changes made to 29889187aa03b8678b439b2747b671e42ff5fa1b.

--- src/main/java/project_16x16/Constants.java
+++ src/main/java/project_16x16/Constants.java
@@ -19,6 +19,8 @@ public final class Constants {
 	public static final float CAMERA_ZOOM_MIN = 0.3f;
 	public static final float GAME_GRAVITY = 1;
 
+	public static final String LEVEL_1 = "Storage/Game/Maps/save/levEl1.dat";
+
 	public static final String GAME_FONT = "Font/font-pixel-48.vlw";
 	
 	/**
@@ -27,8 +29,8 @@ public final class Constants {
 	public static final String DEV_LEVEL = "Storage/Game/Maps/gg-2.dat";
 
 	//game playing levels
-	public static final String LEVEL1 = "Storage/Game/Maps/save/levEl1.dat";
-	public static final String LEVEL2 = "Storage/Game/Maps/save/lEVEL2.dat";
+	public static final String LEVEL1 = "Storage/Game/Maps/save/level1.dat";
+	public static final String LEVEL2 = "Storage/Game/Maps/save/level2.dat";
 	public static final String LEVEL3 = "Storage/Game/Maps/save/level3.dat";
 
 	/**
